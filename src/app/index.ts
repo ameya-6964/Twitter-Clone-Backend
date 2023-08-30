0c1a19217eb42965049161e1337532666425284e
ameya-6964 <belvalkar7@gmail.com>
2023-08-30 20:33:10 +0530

    Added Authentication For Token

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,18 +1,24 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import bodyParser from "body-parser";
+import { User } from "./user";
 import { expressMiddleware } from "@apollo/server/express4";
 
 export async function initServer() {
   const app = express();
   app.use(bodyParser.json());
   const graphqlServer = new ApolloServer({
-    typeDefs: `type Query
-               {
-                 sayHello: String
-               }`,
+    typeDefs: `
+       ${User.types}
+
+        type Query {
+            ${User.queries}
+        }
+    `,
     resolvers: {
-      Query: { sayHello: () => `Hello From GraphQl Server` },
+      Query: {
+        ...User.resolvers.queries,
+      },
     },
   });
   await graphqlServer.start();
---
+++ src/app/user/resolvers.ts
@@ -0,0 +1,27 @@
+interface GoogleTokenResult {
+  iss?: string;
+  nbf?: string;
+  aud?: string;
+  sub?: string;
+  email: string;
+  email_verified: string;
+  azp?: string;
+  name?: string;
+  picture?: string;
+  given_name: string;
+  family_name?: string;
+  iat?: string;
+  exp?: string;
+  jti?: string;
+  alg?: string;
+  kid?: string;
+  typ?: string;
+}
+
+const queries = {
+  verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
+    return token;
+  },
+};
+
+export const resolvers = { queries };
